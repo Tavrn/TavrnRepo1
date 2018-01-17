a3beacc16caf5d842861b9d399157df6398b4a29
Tavrn <33767561+Tavrn@users.noreply.github.com>
2018-01-16 19:00:33 -0800

    Alpha_v1

---
+++ Arcane Arcade - Copy/Arcane Arcade - Copy/Assets/Scripts/BroomScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroomScript : MonoBehaviour {
+	public GameObject player;
+	public GameObject head;
+	// public GameObject headReferenceObject;
+	// private HeadReference headReference;
+	private float currentPressure=0f;
+	private int acceleration = 10;
+	private float broomHeight = 176.88f;
+	private bool isMounted = false;
+	private Rigidbody prb;
+	private int yrotatespeed = 1;
+	private float zrotatespeed = 0.5f;
+	private Vector3 mountLocation;
+	private int maxSpeed = 15;
+	private float maxDeviate = 0.1f;
+	// Use this for initialization
+	void Start () {
+		prb = player.GetComponent<Rigidbody>();
+		// headReference = headReferenceObject.GetComponent<HeadReference>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void FixedUpdate(){
+		if(isMounted){
+			Vector3 difference = head.transform.localPosition-mountLocation;
+			float speed = prb.velocity.magnitude;
+			if(difference.x<maxDeviate){
+				player.transform.RotateAround(player.transform.position, player.transform.up, yrotatespeed*difference.x*speed);
+			}else{
+				player.transform.RotateAround(player.transform.position, player.transform.up, yrotatespeed*maxDeviate*speed);
+			}
+			if(difference.z<maxDeviate){
+				player.transform.RotateAround(player.transform.position, player.transform.right, zrotatespeed*difference.z*speed);
+			}else{
+				player.transform.RotateAround(player.transform.position, player.transform.right, zrotatespeed*maxDeviate*speed);
+			}
+			prb.AddForce(player.transform.forward*acceleration*currentPressure);
+			prb.velocity = transform.forward*prb.velocity.magnitude;
+			if(prb.velocity.magnitude>maxSpeed){
+				prb.velocity = transform.forward*maxSpeed;
+			}
+		}
+	}
+	public void UpdateCurrentPressure(float f){
+		currentPressure = f;
+	}
+	void OnTriggerEnter(Collider c){
+		if(c.gameObject.tag != "Town"){
+			Mount();
+
+			// headReference.Mount();
+		}
+	}
+	public void Mount(){
+		if(!isMounted){
+			Debug.Log("mounted");
+			mountLocation = head.transform.localPosition;
+			isMounted = true;
+			transform.parent = player.transform;
+			transform.position = new Vector3(head.transform.position.x, broomHeight, head.transform.position.z);
+		}
+	}
+}
---
+++ Arcane Arcade - Copy/Arcane Arcade - Copy/Assets/Scripts/SpellManagerScript.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CNamespace;
+public class SpellManagerScript : MonoBehaviour {
+	public Transform wandTip;
+	public Transform wandHandle;
+	public WandScript wandScript;
+	[Space(10)]
+	public Transform minionSpawn1;
+	public Transform minionSpawn2;
+	public GameObject minionPrefab;
+	[Space(10)]
+	public Skybox playerSkybox;
+	public Material clearSkiesSkybox;
+	public Material fireSkybox;
+	[Space(10)]
+	public GameObject fireballPrefab;
+	private int cuedSpellNum = -1;
+	List<Coordinate> fireball = new List<Coordinate>();
+	List<Coordinate> airslash = new List<Coordinate>();
+	List<Coordinate> spawnMinion = new List<Coordinate>();
+	List<Coordinate> weatherClear = new List<Coordinate>();
+	List<Coordinate> weatherFire = new List<Coordinate>();
+	List<List<Coordinate>> spellCompendium = new List<List<Coordinate>>();
+	List<string> spellNameCompendium = new List<string>();
+	void Start () {
+		setUpSpellCompendium();
+		setUpSpellNameCompendium();
+	}
+	void setUpSpellNameCompendium(){
+		spellNameCompendium.Add("Fireball");
+		spellNameCompendium.Add("AirSlash");
+		spellNameCompendium.Add("I_SpawnMinion");
+		spellNameCompendium.Add("I_WeatherClear");
+		spellNameCompendium.Add("I_WeatherFire");
+	}
+	void setUpSpellCompendium(){
+		fireball.Add(new Coordinate(0,0,0));
+		fireball.Add(new Coordinate(0,1,0));
+		spellCompendium.Add(fireball);
+		airslash.Add(new Coordinate(0,0,0));
+		airslash.Add(new Coordinate(0,-1,0));
+		spellCompendium.Add(airslash);
+		spawnMinion.Add(new Coordinate(0,0,0));
+		spawnMinion.Add(new Coordinate(0,0,1));
+		spawnMinion.Add(new Coordinate(0,0,2));
+		spellCompendium.Add(spawnMinion);
+		weatherClear.Add(new Coordinate(0,0,0));
+		weatherClear.Add(new Coordinate(1,0,0));
+		weatherClear.Add(new Coordinate(1,1,0));
+		weatherClear.Add(new Coordinate(2,1,0));
+		spellCompendium.Add(weatherClear);
+		weatherFire.Add(new Coordinate(0,0,0));
+		weatherFire.Add(new Coordinate(-1,0,0));
+		weatherFire.Add(new Coordinate(-1,1,0));
+		weatherFire.Add(new Coordinate(-2,1,0));
+		spellCompendium.Add(weatherFire);
+	}
+	public void checkForSpell(){
+		List<Coordinate> pattern = wandScript.pattern;
+		if(pattern.Count>0){ //make sure the origin collider is in the pattern list
+			if(pattern[0].getX()!=0 || pattern[0].getY() != 0 || pattern[0].getZ()!=0){
+				pattern.Insert(0, new Coordinate(0,0,0));
+			}
+		}
+		for(int spellNum = 0; spellNum<spellCompendium.Count; spellNum++){ //check pattern against every spell in the compendium
+			if(compareSpells(spellCompendium[spellNum], pattern)){
+				GetComponent<AudioSource>().Play();
+				string temp = spellNameCompendium[spellNum];
+				if(temp[0] == 'I' && temp[1] == '_'){ //check if spell is instantly cast or not
+					Invoke(spellNameCompendium[spellNum], 0f);
+					cuedSpellNum = -1;
+				}else{
+					cuedSpellNum = spellNum;
+				}
+			}
+		}
+		wandScript.pattern.Clear();
+	}
+	bool compareSpells(List<Coordinate> spell1, List<Coordinate> spell2){
+		Debug.Log("Counts: " + spell1.Count + " " + spell2.Count);
+		if(spell1.Count!=spell2.Count){
+			return false;
+		}else{
+			bool result = true;
+			for(int i=0; i<spell1.Count; i++){
+				if(!compareCoordinates(spell1[i], spell2[i])){
+					result = false;
+				}
+			}
+			return result;
+		}
+	}
+	bool compareCoordinates(Coordinate a, Coordinate b){
+		// Debug.Log(a.getX() + " " + b.getX() + " " + a.getY() + " " + b.getY() + " " + a.getZ() + " " + b.getZ());
+		return a.getX()==b.getX() && a.getY() == b.getY() && a.getZ() == b.getZ();
+	}
+	public void FireSpell(){
+		if(cuedSpellNum!=-1){
+			Invoke(spellNameCompendium[cuedSpellNum], 0f);
+			cuedSpellNum = -1;
+		}
+	}
+	void Fireball(){
+		Debug.Log("Fireball called");
+		Vector3 dir = wandTip.position-wandHandle.position;
+		GameObject fb = Instantiate(fireballPrefab) as GameObject;
+		fb.transform.position = wandTip.position+dir;
+		fb.GetComponent<Rigidbody>().AddForce(dir.normalized*10);
+	}
+	void AirSlash(){
+		Debug.Log("Air slash called");
+	}
+	void I_SpawnMinion(){
+		Debug.Log("spawn minion called");
+		GameObject minion = Instantiate(minionPrefab) as GameObject;
+		minion.transform.position = minionSpawn1.position;
+	}
+	void I_WeatherClear(){
+		playerSkybox.material = clearSkiesSkybox;
+	}
+	void I_WeatherFire(){
+		playerSkybox.material = fireSkybox;
+	}
+}
